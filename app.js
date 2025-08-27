2b9e9ed0a2b93b74c633ec7bde9d3f8699f735f7
Saksham goyal <229024266+goyalsaksham40@users.noreply.github.com>
2025-08-28 04:52:22 +0530

    DELETE app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const $ = (id) => document.getElementById(id);
 const navToggle = document.querySelector('.nav-toggle');
 const nav = document.getElementById('nav');
 if (navToggle) {
-  navToggle.addEventListener('click', () => {
+  navToggle.addEventListener('click', () => 
     const open = nav.classList.toggle('open');
     navToggle.setAttribute('aria-expanded', open);
   });
